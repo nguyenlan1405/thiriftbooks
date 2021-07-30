6625ae1c9d675160296295f3f25b8cedda5af473
Lan Nguyen <lannguyenthb@gmail.com>
2021-07-31 00:40:52 +0700

    add menu

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -15,3 +15,14 @@ endif;
 add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
 
 // END ENQUEUE PARENT ACTION
+
+function my_wp_nav_menu_args( $args = '' ) {
+ 
+if( is_user_logged_in() ) { 
+    $args['menu'] = 'menulogout';
+} else { 
+    $args['menu'] = 'menulogin';
+} 
+    return $args;
+}
+add_filter( 'wp_nav_menu_args', 'my_wp_nav_menu_args' );
