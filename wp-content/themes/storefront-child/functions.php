212d4058b709ed11f6f645ad2047eb22a31163a9
Lan Nguyen <lannguyenthb@gmail.com>
2021-09-03 16:35:38 +0700

    edit file

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -19,10 +19,16 @@ add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
 function my_wp_nav_menu_args( $args = '' ) {
  
  if( is_user_logged_in() ) { 
-     $args['menu'] = 'menulogout';
+ 	if( 'top-navigation' == $args['theme_location'] ) { // Change top-navigation to theme specific name
+	    $args['menu'] = 'menulogout';
+	}
+     
  } else { 
-     $args['menu'] = 'menulogin';
- } 
+	if( 'top-navigation' == $args['theme_location'] ) { // Change top-navigation to theme specific name
+	    $args['menu'] = 'menulogin';
+	}
+     
+	} 
      return $args;
  }
  add_filter( 'wp_nav_menu_args', 'my_wp_nav_menu_args' );
